156d4d41f3f45adc9abfe9d4d36ff7220bc2213a
Faiz Yoshio <faizyoshio@gmail.com>
2026-02-12 15:56:22 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="w-full py-6 sm:py-8 px-4 text-center text-xs sm:text-sm text-foreground/50 hover:text-foreground/70 transition-colors duration-300">
-      <p>© 2019 FaizYoshio.my.id. All Rights Reserved.</p>
+      <p>© 2019 Faiz Yoshio. All Rights Reserved.</p>
     </footer>
   )
 }
